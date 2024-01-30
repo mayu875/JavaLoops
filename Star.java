f58c7ff260a6dd28d3a6456ea5d7da4090637170
mayu875 <shivangekarmayuri@gmail.com>
2024-01-30 20:28:25 +0530

    Star Commit

---
+++ Star.java
@@ -0,0 +1,39 @@
+
+public class Star {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int j;
+		int row=10;
+		for(i=1;i<=row;i++) {
+			for(j=row;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+...........................................................................................................
+public class Star1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int j;
+		for(i=1;i<=10;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+
+

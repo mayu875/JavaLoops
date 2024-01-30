4223cca9ae5efe26ac8b227a577a52c6dbba7b5f
mayu875 <shivangekarmayuri@gmail.com>
2024-01-30 20:27:54 +0530

    Loop Number Commit

---
+++ Number.java
@@ -0,0 +1,30 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1; i<=10;i++) {
+			System.out.println(i);
+	
+		}
+
+	}
+
+}
+............................................................................................................................
+public class Number1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int j;
+		for(i=10;i>=1;i--) {
+			for(j=i;j>=1;j--) {
+				System.out.println(j+" ");
+			}
+		}
+
+	}
+
+}
